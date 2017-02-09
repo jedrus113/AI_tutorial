fae838b422558a301d87d15fc5233d3911db13f4
andrew <jedrus1133@gmail.com>
2017-02-10 00:41:40 +0100

    hope code it correctly, filrs tutorial result

---
+++ neural_network.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class Neural_Network(object):
+    def __init(self):
+        #Define Hyperparameters
+        self.inputLayerSize = 2
+        self.outputLayerSize = 1
+        self.hiddenLayerSize =3
+
+        #Weights (Parameters)
+        self.W1 = np.random.randn(self.inputLayerSize, self.hiddenLayerSize)
+        self.W2 = np.random.randn(self.hiddenLayerSize, self.outputLayerSize)
+
+    def forward(self, X):
+        #Propagate inputs though network
+        self.z2 = np.dot(X, self.W1)
+        self.a2 = self.sigmoid(self.z2)
+        self.x3 = np.dot(self.a2, self.W2)
+        yHat = self.sigmoid(self.z3)
+
+
+    def sigmoid(self, z):
+        #Apply sigmoid activation function to scalar, vector, or matrix
+        return 1/(1+np.exp(-z))
